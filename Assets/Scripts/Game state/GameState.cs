a50ce70b34a5b08afb21b907b2fb273afd6a0fcb
Crenantious <66210196+Crenantious@users.noreply.github.com>
2023-03-15 16:23:49 +0000

    Add GameState and update assembly namespaces.

---
+++ Assets/Scripts/Game state/GameState.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LittleKingdom.GameState
+{
+    public class GameState
+    {
+        public void Load()
+        {
+
+        }
+    }
+}
