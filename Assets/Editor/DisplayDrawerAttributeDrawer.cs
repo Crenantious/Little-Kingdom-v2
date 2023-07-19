1c06706456503c5acceb6b87bc683e5acc25940a
Crenantious <66210196+Crenantious@users.noreply.github.com>
2023-07-19 04:52:51 +0100

    Fix issue with DisplayDrawer not updating lists correctly.

--- Assets/Editor/DisplayDrawerAttributeDrawer.cs
+++ Assets/Editor/DisplayDrawerAttributeDrawer.cs
@@ -4,12 +4,9 @@ using LittleKingdom.Attributes;
 
 namespace LittleKingdom
 {
-    //TODO : JR - when a list's foldout is inactive, the elements do not get hidden.
     [CustomPropertyDrawer(typeof(DisplayDrawerAttribute), true)]
     public class DisplayDrawerAttributeDrawer : PropertyDrawer
     {
-        private UnityEditor.Editor editor = null;
-
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             if (property.objectReferenceValue)
@@ -27,17 +24,11 @@ namespace LittleKingdom
         private void DrawWithNotNullObject(Rect position, SerializedProperty property, GUIContent label)
         {
             property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
-
             EditorGUI.indentLevel++;
-
             EditorGUI.PropertyField(position, property, label, true);
 
             if (property.isExpanded)
-            {
-                if (!editor)
-                    UnityEditor.Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
-                editor.OnInspectorGUI();
-            }
+                UnityEditor.Editor.CreateEditor(property.objectReferenceValue).OnInspectorGUI();
 
             EditorGUI.indentLevel--;
         }
